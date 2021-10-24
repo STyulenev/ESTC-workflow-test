c58e2dbb700a95556c69333ee85caf1ac769449c
Tyulenev Sergey <tyulenev.i@yandex.ru>
2021-10-24 15:25:25 +0300

    Added the function of element-by-element substraction of two 3D vectors.

--- module_3d.c
+++ module_3d.c
@@ -6,3 +6,10 @@ void summ_vector_3d(int size, int *** vector_one, int *** vector_two, int *** ve
             for (int k = 0; k < size; ++k)
                 vector_result[i][j][k] = vector_one[i][j][k] + vector_two[i][j][k];
 }
+
+void differece_vector_3d(int size, int *** vector_one, int *** vector_two, int *** vector_result) {
+    for (int i = 0; i < size; ++i)
+        for (int j = 0; j < size; ++j)
+            for (int k = 0; k < size; ++k)
+                vector_result[i][j][k] = vector_one[i][j][k] - vector_two[i][j][k];
+}
